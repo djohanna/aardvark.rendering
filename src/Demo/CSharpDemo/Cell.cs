68743553b707f6b1d824954d917f777651abcd32
krauthaufen <krauthaufen@awx.at>
2016-05-17 18:29:47 +0200

    sync

---
+++ src/Demo/CSharpDemo/Cell.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aardvark.Base;
+
+namespace CSharpDemo
+{
+    /// <summary>
+    /// A 2^Exponent sized cube positioned at (X,Y,Z) * 2^Exponent.
+    /// </summary>
+    public struct Cell : IEquatable<Cell>
+    {
+        public readonly long X;
+        public readonly long Y;
+        public readonly long Z;
+        public readonly int Exponent;
+
+        /// <summary>
+        /// Cell with min corner at (x,y,z)*2^exponent and dimension 2^exponent.
+        /// </summary>
+        public Cell(long x, long y, long z, int exponent)
+        {
+            X = x; Y = y; Z = z; Exponent = exponent;
+            BoundingBox = ComputeBoundingBox(X, Y, Z, Exponent);
+        }
+
+        /// <summary>
+        /// Cell with min corner at index*2^exponent and dimension 2^exponent.
+        /// </summary>
+        public Cell(V3l index, int exponent) : this(index.X, index.Y, index.Z, exponent) { }
+
+        /// <summary>
+        /// Special cell, which is centered at origin, with dimension 2^exponent.
+        /// </summary>
+        public Cell(int exponent) : this(long.MaxValue, long.MaxValue, long.MaxValue, exponent) { }
+
+        /// <summary>
+        /// Smallest cell that contains given box.
+        /// </summary>
+        public Cell(Box3d box)
+        {
+            if (!box.IsValid || box.Min == box.Max) throw new InvalidOperationException();
+
+            // case 1: contains origin
+            if (box.Min.X < 0.0 && box.Max.X > 0.0 ||
+                box.Min.Y < 0.0 && box.Max.Y > 0.0 ||
+                box.Min.Z < 0.0 && box.Max.Z > 0.0)
+            {
+                X = Y = Z = long.MaxValue;
+                Exponent = Math.Max(box.Min.NormMax, box.Max.NormMax).Log2Int() + 1;
+            }
+            else // case 2: doesn't contain origin
+            {
+                Exponent = box.Size.NormMax.Log2Int();
+                var s = Math.Pow(2.0, Exponent);
+                var a = (box.Min / s).Floor * s;
+                while (a.X + s < box.Max.X || a.Y + s < box.Max.Y || a.Z + s < box.Max.Z)
+                {
+                    s *= 2.0; Exponent++;
+                    a = (box.Min / s).Floor * s;
+                }
+                X = (long)Math.Floor(a.X / s);
+                Y = (long)Math.Floor(a.Y / s);
+                Z = (long)Math.Floor(a.Z / s);
+            }
+
+            BoundingBox = ComputeBoundingBox(X, Y, Z, Exponent);
+        }
+
+        /// <summary>
+        /// Gets whether this cell is a special cell centered at origin.
+        /// </summary>
+        public bool IsCenteredAtOrigin => X == long.MaxValue && Y == long.MaxValue && Z == long.MaxValue;
+
+        /// <summary>
+        /// Returns true if the cell completely contains the other cell.
+        /// </summary>
+        public bool Contains(Cell other)
+        {
+            return BoundingBox.Contains(other.BoundingBox);
+
+            // !!! below: ~8x speedup, but is it correct? ;-)
+            // !!! does not handle special cells centered at origin
+            //if (other.Exponent > Exponent) return false;
+            //if (other.Exponent == Exponent) return other.X == X && other.Y == Y && other.Z == Z;
+            //var d = 1 << (Exponent - other.Exponent);
+            //var x0 = X * d; var y0 = Y * d; var z0 = Z * d;
+            //var x1 = x0 + d; var y1 = y0 + d; var z1 = z0 + d;
+            //return other.X >= x0 && other.X < x1 && other.Y >= y0 && other.Y < y1 && other.Z >= z0 && other.Z < z1;
+        }
+
+        /// <summary>
+        /// Returns true if two cells intersect each other.
+        /// </summary>
+        public bool Intersects(Cell other)
+        {
+            return BoundingBox.Intersects(other.BoundingBox);
+        }
+
+        /// <summary>
+        /// Gets indices of the 8 subcells.
+        /// </summary>
+        public Cell[] Children
+        {
+            get
+            {
+                var x0 = X << 1; var y0 = Y << 1; var z0 = Z << 1;
+                var x1 = x0 + 1; var y1 = y0 + 1; var z1 = z0 + 1;
+                var e = Exponent - 1;
+                return new[]
+                {
+                    new Cell(x0, y0, z0, e),
+                    new Cell(x1, y0, z0, e),
+                    new Cell(x0, y1, z0, e),
+                    new Cell(x1, y1, z0, e),
+                    new Cell(x0, y0, z1, e),
+                    new Cell(x1, y0, z1, e),
+                    new Cell(x0, y1, z1, e),
+                    new Cell(x1, y1, z1, e),
+                };
+            }
+        }
+
+        /// <summary>
+        /// Gets cell's bounds.
+        /// </summary>
+        public Box3d BoundingBox { get; }
+
+        /// <summary>
+        /// Octant 0-7.
+        /// 0th, 1st and 2nd bit encodes x-, y-, z-axis, respectively.
+        /// E.g. 0 is octant at origin, 7 is octant oposite from origin.
+        /// </summary>
+        public Cell GetOctant(int i)
+        {
+            if (i < 0 || i > 7) throw new IndexOutOfRangeException();
+            return new Cell(
+                (X << 1) + ((i & 1) == 0 ? 0 : 1),
+                (Y << 1) + ((i & 2) == 0 ? 0 : 1),
+                (Z << 1) + ((i & 4) == 0 ? 0 : 1),
+                Exponent - 1
+                );
+        }
+
+        #region equality and hashing
+
+        public bool Equals(Cell other) => this == other;
+        public static bool operator ==(Cell a, Cell b) => a.X == b.X && a.Y == b.Y && a.Z == b.Z && a.Exponent == b.Exponent;
+        public static bool operator !=(Cell a, Cell b) => !(a == b);
+        public override bool Equals(object obj) => obj is Cell && this == (Cell)obj;
+        public override int GetHashCode() => HashCode.GetCombined(X, Y, Z, Exponent);
+
+        #endregion
+
+
+        public override string ToString()
+        {
+            return $"[{X}, {Y}, {Z}, {Exponent}]";
+        }
+
+        private static Box3d ComputeBoundingBox(long x, long y, long z, int e)
+        {
+            var d = Math.Pow(2.0, e);
+            var isCenteredAtOrigin = x == long.MaxValue && y == long.MaxValue && z == long.MaxValue;
+            var min = isCenteredAtOrigin ? new V3d(-0.5 * d) : new V3d(x * d, y * d, z * d);
+            return Box3d.FromMinAndSize(min, new V3d(d, d, d));
+        }
+    }
+}
--- src/Demo/Examples/Optimization.fs
+++ src/Demo/Examples/Optimization.fs
@@ -12,6 +12,300 @@ open Aardvark.Base.Incremental.Operators
 open Aardvark.SceneGraph.Semantics
 open Aardvark.Rendering.NanoVg
 
+module TreeStuff =
+
+    type Value<'a>(create : unit -> 'a) =
+        let mutable cache = None
+
+        member x.Value =
+            lock x (fun () ->
+                match cache with
+                    | Some c -> c
+                    | None -> 
+                        let c = create()
+                        cache <- Some c
+                        c
+            )
+
+        member x.Destroy() =
+            lock x (fun () ->
+                cache <- None
+            )
+
+    module Value =
+        let create (f : unit -> 'a) = Value f
+        let force (v : Value<'a>) = v.Value
+
+    type ValueBuilder() =
+        member x.Bind(v : Value<'a>, f : 'a -> Value<'b>) =
+            Value(fun () -> v.Value |> f |> Value.force)
+
+        member x.Return(v : 'a) = Value.create (fun () -> v)
+
+        member x.Delay(f : unit -> Value<'a>) = Value(fun () -> f().Value)
+    
+    let value = ValueBuilder()
+
+
+    type Point =
+        struct
+            val mutable public Position : V3f
+            val mutable public Color : C4b
+
+            new(p,c) = { Position = p; Color = c }
+        end
+
+    type Cell = CSharpDemo.Cell
+
+    let div2Floor (v : int64) =
+        if v % 2L = 0L then v / 2L
+        else (v - 1L) / 2L
+
+    let div2AZ (v : int64) =
+        if v % 2L = 0L then v / 2L
+        elif v > 0L then (v + 1L) / 2L
+        else (v - 1L) / 2L
+
+    type GridCell =
+        struct
+            val mutable public X : int64
+            val mutable public Y : int64
+            val mutable public Z : int64
+            val mutable public Exp : int
+
+            member x.BoundingBox =
+                let size = pown 2.0 x.Exp
+
+                let shift = 
+                    let ehalf = floor (float (x.Exp + 1) * 0.5) |> int
+                    let shift = -((pown 4.0 ehalf) - 1.0) / 3.0
+                    shift / size
+
+                Box3d(
+                    (shift + float x.X) * size          |> float,
+                    (shift + float x.Y) * size          |> float,
+                    (shift + float x.Z) * size          |> float,
+                    (shift + float x.X + 1.0) * size    |> float,
+                    (shift + float x.Y + 1.0) * size    |> float,
+                    (shift + float x.Z + 1.0) * size    |> float
+                )
+
+
+            member x.Parent =
+                if x.Exp % 2 = 0 then
+                    GridCell(div2Floor (1L + x.X), div2Floor (1L + x.Y), div2Floor (1L + x.Z), x.Exp + 1)
+                else
+                    GridCell(div2Floor x.X, div2Floor x.Y, div2Floor x.Z, x.Exp + 1)
+
+            override x.ToString() =
+                sprintf "GridCell(%dL, %dL, %dL, %d)" x.X x.Y x.Z x.Exp
+
+            new(x,y,z,e) = { X = x; Y = y; Z = z; Exp = e }
+            new(x : int,y : int,z : int,e : int) = { X = int64 x; Y = int64 y; Z = int64 z; Exp = e }
+        end
+
+    module Check =
+        open System
+
+        let parentContains (c : GridCell) =
+            c.Parent.BoundingBox.Contains c.BoundingBox
+
+        let printBad (cell : GridCell) =
+            let self = cell.BoundingBox
+            let parent = cell.Parent.BoundingBox
+
+            printfn "cell:   %A" cell
+            printfn "self:   %A" self
+            printfn "parent: %A" parent
+
+
+        let test() =
+            let r = Random()
+
+            let randomIndex() = r.Next(1 <<< 16) - 1 <<< 15
+            let randomExp() = (r.Next(32) - 16) //- 16
+
+            let randomCell () = GridCell(randomIndex(), randomIndex(), randomIndex(), randomExp())
+
+            for i in 0..1000000 do
+                let cell = randomCell()
+                if not (parentContains cell) then
+                    printBad cell
+                    failwith "asdasdasd"
+
+
+    type Node =
+        | Empty 
+        | Leaf of int * Value<Point[]>
+        | Node of int * Value<Point[]> * Value<Node>[]
+
+    type Octree = { splitThreshold : int; bounds : Box3d; cell : Cell; root : Node }
+
+
+
+    [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+    module Node =
+        
+        let count (n : Node) =
+            match n with
+                | Empty -> 0
+                | Leaf(c,_) -> c
+                | Node(c,_,_) -> c
+
+        let childBoxes (bounds : Box3d) =
+            let l = bounds.Min
+            let c = bounds.Center
+            let h = bounds.Max
+            [|
+                Box3d(l.X, l.Y, l.Z, c.X, c.Y, c.Z)
+                Box3d(l.X, l.Y, c.Z, c.X, c.Y, h.Z)
+                Box3d(l.X, c.Y, l.Z, c.X, h.Y, c.Z)
+                Box3d(l.X, c.Y, c.Z, c.X, h.Y, h.Z)
+                Box3d(c.X, l.Y, l.Z, h.X, c.Y, c.Z)
+                Box3d(c.X, l.Y, c.Z, h.X, c.Y, h.Z)
+                Box3d(c.X, c.Y, l.Z, h.X, h.Y, c.Z)
+                Box3d(c.X, c.Y, c.Z, h.X, h.Y, h.Z)
+            |]
+
+        let rec build (splitThreshold : int) (box : Box3d) (points : Point[]) : Node =
+            if points.Length = 0 then
+                Empty
+
+            elif points.Length < splitThreshold then
+                Leaf(points.Length, value { return points })
+
+            else
+                let childBoxes = childBoxes box
+
+                let childData = 
+                    childBoxes |> Array.map (fun b ->
+                        points |> Array.filter (fun p -> b.Contains (V3d p.Position))
+                    )
+
+                let children =
+                    Array.init 8 (fun i ->
+                        value {
+                            return build splitThreshold childBoxes.[i] childData.[i]
+                        }
+                    )
+
+                Node(points.Length, value { return [||]}, children)
+
+            
+
+    [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+    module Octree =
+        
+        let count (t : Octree) =
+            t.root |> Node.count
+        
+        let isEmpty (t : Octree) =
+            match t.root with
+                | Empty -> true
+                | _ -> false
+
+        let empty (splitThreshold : int) = { splitThreshold = splitThreshold; bounds = Unchecked.defaultof<Box3d>; cell = Unchecked.defaultof<Cell>; root = Empty }
+
+
+        let addContained (points : Point[]) (t : Octree) =
+            let rec add (bounds : Box3d) (points : Point[]) (n : Node) =
+                match n with
+                    | Empty -> 
+                        Node.build t.splitThreshold bounds points
+
+                    | Leaf(cnt, data) ->
+                        if cnt + points.Length < t.splitThreshold then
+                            Leaf (
+                                cnt + points.Length,
+                                value {
+                                    let! data = data
+                                    return Array.append points data
+                                }
+                            )
+                        else
+                            Node.build t.splitThreshold bounds points
+
+                    | Node(cnt, data, children) ->
+                        let childBoxes = Node.childBoxes bounds
+
+                        let childData = 
+                            childBoxes |> Array.map (fun b ->
+                                points |> Array.filter (fun p -> b.Contains (V3d p.Position))
+                            )
+
+                        let newChildren =
+                            Array.map3 (fun b node d ->
+                                value {
+                                    let! node = node
+                                    return add b d node
+                                }
+                            ) childBoxes children childData
+
+                        Node(cnt + points.Length, data, newChildren)
+
+            { t with root = add t.bounds points t.root}
+
+        let add (points : Point[]) (t : Octree) =
+            if isEmpty t then
+                let bounds = Box3d(points |> Seq.map (fun p -> V3d p.Position))
+                let cell = Cell(bounds)
+                { t with bounds = bounds; cell = cell; root = Node.build t.splitThreshold cell.BoundingBox points }
+
+            else
+                let inside, outside = points |> Array.partition (fun p -> t.bounds.Contains (V3d p.Position))
+                let t = addContained inside t
+
+                if outside.Length = 0 then
+                    t
+                else
+                    let bounds = Box3d(points |> Seq.map (fun p -> V3d p.Position))
+
+                    let root = t.root
+                    let cnt = Node.count root
+
+                    let rec createNode (cell : Cell) =
+                        if cell = t.cell then
+                            t.root
+
+                        elif cell.Contains t.cell then
+                            let children = t.cell.Children
+                            Node(
+                                cnt,
+                                value { return [||] },
+                                Array.init 8 (fun i ->
+                                    value { return createNode children.[i] }
+                                )
+                            )
+
+                        else
+                            Empty
+
+                    let newCell = Cell(bounds.Union t.cell.BoundingBox)
+                    let newRoot = createNode newCell
+
+                    addContained outside { 
+                        t with 
+                            cell = newCell
+                            bounds = bounds.Union t.bounds
+                            root = newRoot 
+                        }
+
+                        
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let run() =
     Aardvark.Init()
 
